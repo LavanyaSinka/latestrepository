6b28de17217bf7e18b86845bd838f2992c90e8a0
Lavanya <159117767+LavanyaSinka@users.noreply.github.com>
2024-02-15 07:25:07 +0530

    Update deployEc2Instance.rb

--- ec2deployment/recipes/deployEc2Instance.rb
+++ ec2deployment/recipes/deployEc2Instance.rb
@@ -10,19 +10,19 @@ end
 
 include recipe 'aws'
 aws_creds=data_bag_item('aws', 'credentials')
-aws_access_key_id= aws_creds['AKIATCKASWIER7UWXEA7']
-aws_secret_key_id= aws_creds['SolmUYqxPHMUKs0jctKSdjHL2lqwgFHD5/pv4c3E']
+aws_access_key_id= aws_creds['Enter your access key']
+aws_secret_key_id= aws_creds['Enter your secret key']
 aws_region= 'ap-south-1a'
 
 aws_instance 'Lavanya-first-ec2instance-using-chef' do
 aws_access_key_id aws_access_key_id
 aws_secret_key_id aws_secret_key_id
 region aws_region
-image id 'ami-0e731c8a588258d0d'
+image id 'Enter your image id'
 instance_type 't2.micro'
 key_name 'Lavany-key'
-subnet_id 'subnet-02d25feb674b62ccb'
-securitygroup_id 'sg-0877e27c83573d892'
+subnet_id 'Enter Subnet id'
+securitygroup_id 'Enter security group id'
 
 action :create
 end
